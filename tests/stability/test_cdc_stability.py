c028fcf732548a994ea5d75e20fb7d56d6a7f68d
zhuwenxing <wenxing.zhu@zilliz.com>
2023-04-12 20:11:42 +0800

    update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- tests/stability/test_cdc_stability.py
+++ tests/stability/test_cdc_stability.py
@@ -1,5 +1,6 @@
 import time
 import math
+from multiprocessing import Process
 from datetime import datetime
 from utils.util_log import test_log as log
 from api.milvus_cdc import MilvusCdcClient
@@ -23,6 +24,31 @@ def divide_time(total_time, num_segments=10):
         segment_time = math.exp(i - 1) / sum([math.exp(j - 1) for j in range(1, num_segments + 1)]) * total_time
         segment_times.append(segment_time)
     return segment_times
+
+
+def get_count_by_query(host, port, c_name):
+    from pymilvus import connections
+    from pymilvus import Collection
+    connections.connect(host=host, port=port)
+    timeout = 60
+    t0 = time.time()
+    while True and time.time() - t0 < timeout:
+        if c_name in list_collections():
+            log.info(f"collection {c_name} is created")
+            break
+        time.sleep(1)
+        if time.time() - t0 > timeout:
+            raise Exception(f"Timeout waiting for collection {c_name} to be created")    
+    col = Collection(name=c_name)
+    index_infos = [index.to_dict() for index in col.indexes]
+    if len(index_infos) == 0:
+        col.create_index(field_name="float_vector",
+                        index_params={"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}})
+    col.load()
+    while True:
+        count_by_query_downstream = len(col.query(expr="int64 >= 0", output_fields=["int64"]))
+        log.info(f"count_by_query_downstream: {count_by_query_downstream}")
+        time.sleep(60)
 
 
 class TestCdcStability(TestBase):
@@ -76,78 +102,8 @@ class TestCdcStability(TestBase):
             delete_checker = DeleteEntitiesCollectionChecker(host=upstream_host, port=upstream_port, c_name=c_name)
             delete_checker.run()
             delete_checker_list.append(delete_checker)
-        checker_list = list(zip(insert_checker_list, delete_checker_list))
-        for i in range(check_times):
-            log.info(f"start to check entities in upstream and downstream, {i}th time, sleep time: {sleep_time_list[i]}")
-
-            time.sleep(sleep_time_list[i])
-            log.info("start to pause all checkers")
-            for i, checker in enumerate(checker_list):
-                insert_checker, delete_checker = checker
-                insert_checker.pause()
-                delete_checker.pause()                
-            log.info("start to check entities in upstream and downstream")
-            for i, checker in enumerate(checker_list):
-                c_name = c_name_list[i]
-                insert_checker, delete_checker = checker
-                insert_checker.pause()
-                delete_checker.pause()
-                # check entities in upstream
-                num_entities_upstream = insert_checker.get_num_entities()
-                log.info(f"num_entities in upstream: {num_entities_upstream}")
-                count_by_query_upstream = insert_checker.get_count_by_query()
-                log.info(f"count_by_query in upstream: {count_by_query_upstream}")
-
-                log.info("start to connect to downstream")
-                # check entities in downstream
-                connections.disconnect("default")
-                log.info("disconnect default")
-                connections.connect(host=downstream_host, port=downstream_port)
-                log.info("connect to downstream")
-                timeout = 60
-                t0 = time.time()
-                while True and time.time() - t0 < timeout:
-                    if c_name in list_collections():
-                        log.info(f"collection {c_name} is created")
-                        break
-                    time.sleep(1)
-                    if time.time() - t0 > timeout:
-                        raise Exception(f"Timeout waiting for collection {c_name} to be created")
-                col = Collection(name=c_name)
-                index_infos = [index.to_dict() for index in col.indexes]
-                if len(index_infos) == 0:
-                    col.create_index(field_name="float_vector",
-                                    index_params={"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}})
-                col.load()
-                # wait for the collection to be synced
-                timeout = 120
-                count_by_query_downstream = len(col.query(expr="int64 >= 0", output_fields=["int64"]))
-                t0 = time.time()
-                while True and time.time() - t0 < timeout:
-                    count_by_query_downstream = len(col.query(expr="int64 >= 0", output_fields=["int64"]))
-                    # log.info(f"count_by_query_downstream: {len(count_by_query_downstream)}")
-                    log.info(
-                        f"count_by_query_downstream {count_by_query_downstream},"
-                        f"count_by_query_upstream {count_by_query_upstream}")
-                    if count_by_query_downstream == count_by_query_upstream:
-                        log.info(f"collection {c_name} has been synced")
-                        break
-                    time.sleep(1)
-                    if time.time() - t0 > timeout:
-                        raise Exception(f"Timeout waiting for collection {c_name} to be synced")
-                log.info(f"count_by_query in downstream: {count_by_query_downstream}")
-                assert count_by_query_upstream == count_by_query_downstream
-                # flush collection in downstream
-                col.flush()
-                num_entities_downstream = col.num_entities
-                log.info(f"num_entities in downstream: {num_entities_downstream}")
-                assert num_entities_upstream == num_entities_downstream
-
-            connections.disconnect("default")
-            connections.connect(host=upstream_host, port=upstream_port)
-            for i, checker in enumerate(checker_list):
-                insert_checker, delete_checker = checker
-                insert_checker.resume()
-                delete_checker.resume()     
-
-
+        for c_name in c_name_list:
+            p = Process(target=get_count_by_query, args=(downstream_host, downstream_port, c_name))
+            p.start()
+            p.join()
+        time.sleep(duration_time)
